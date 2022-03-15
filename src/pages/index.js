d7c5cada427db9b369cabe223cc8fbdacef10cc9
PaulZuev <pavel.z@ramotion.agency>
2022-03-15 18:42:04 +0300

    Generation posts on page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,22 @@ import * as React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const GenericPage = props => {
-  console.log("PROPS: ", props)
   const result = useStaticQuery(graphql`
   {
     datoCmsHomepage{
       featuredArticles {
-        originalId
-        slug
-        articleText
-        articleTitle
-        articleShortText
+      ... on DatoCmsSection {
+        id
+        titleSe
+        featuredArticles {
+          articleShortText
+          articleText
+          articleTitle
+          originalId
+          slug
+        }
       }
+    }
       slug
       title
       originalId
@@ -32,33 +37,35 @@ const GenericPage = props => {
     }
   }
   `)
+  console.log(result)
   return (
     <main>
       <div className='md:container md:mx-auto'>
         <h1 className='mt-2 mb-12 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>{result.datoCmsHomepage.title}</h1>
-        <section className='mb-10'>
+        {result.datoCmsHomepage.featuredArticles.map( items => (
+        <section className='mb-10' key={items.id}>
           <div className='md:container md:mx-auto'>
             <h2 className='text-2xl font-extrabold tracking-tight text-gray-900'>
-              {result.datoCmsHomepage.titleSection}
+              {items.titleSe}
             </h2>
           </div>
           <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8'>
-            {result.datoCmsHomepage.featuredArticles.map(featuredArticles => (
-              <div className="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" key={featuredArticles.originalId}>
-                <div className="">
-                  <div className="md:shrink-0">
-                    <img className="h-48 w-full object-cover md:h-40 md:w-full" src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80" alt="Man looking at item at a store"/>
-                  </div>
-                  <div className="p-8">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{featuredArticles.articleTitle}</div>
-                    <p className="mt-2 text-slate-500">{featuredArticles.articleShortText}</p>
-                    <a href="#" className=" mt-3 inline-flex items-center justify-center px-2 py-1 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Read more</a>
+              {items.featuredArticles.map(featuredArticles => (
+                <div className="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" key={featuredArticles.originalId}>
+                  <div className="">
+                    <div className="md:shrink-0">
+                      <img className="h-48 w-full object-cover md:h-40 md:w-full" src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80" alt="Man looking at item at a store"/>
+                    </div>
+                    <div className="p-8">
+                      <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{featuredArticles.articleTitle}</div>
+                      <p className="mt-2 text-slate-500">{featuredArticles.articleShortText}</p>
+                      <a href="#" className=" mt-3 inline-flex items-center justify-center px-2 py-1 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Read more</a>
+                    </div>
                   </div>
                 </div>
-              </div>
             ))}
           </div>
-        </section>
+        </section>))}
         <section className='mb-10'>
           <div className='md:container md:mx-auto'>
             <h2 className='text-2xl font-extrabold tracking-tight text-gray-900'>
@@ -107,7 +114,7 @@ const GenericPage = props => {
                     <a href="#" className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                       <span className="sr-only">Previous</span>
                       <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
                       </svg>
                     </a>
                     <a href="#" aria-current="page" className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"> 1 </a>
@@ -120,7 +127,7 @@ const GenericPage = props => {
                     <a href="#" className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                       <span className="sr-only">Next</span>
                       <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
                       </svg>
                     </a>
                   </nav>
