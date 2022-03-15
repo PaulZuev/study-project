0d90cc06ec67720dfd829512a7269c1d3b405f79
PaulZuev <pavel.z@ramotion.agency>
2022-03-15 10:44:11 +0300

    Fixed

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,29 @@
 
-exports.createPages = async ({ actions, graphql }) => {
-  const {createPage} = actions
-  const result = await graphql(`
-    {
-      allDatoCmsHomepage {
-        nodes {
-          title
-          slug
-          originalId
-          featuredArticles {
-            articleTitle
-            articleText
-          }
-        }
-      }
-    }
-  `)
-   console.log(result) 
-  result.data.allDatoCmsHomepage.nodes.forEach(page =>{
-    createPage({
-      path: `/${page.slug === "index" ? "" : page.slug}`,
-      component: require.resolve("./src/templates/generic-page.js"),
-      context: {
-        pageId: page.originalId,
-      },
-    })
-  })
-}
+// exports.createPages = async ({ actions, graphql }) => {
+//   const {createPage} = actions
+//   const result = await graphql(`
+//     {
+//       datoCmsHomepage{
+//         featuredArticles {
+//           originalId
+//           slug
+//           articleText
+//           articleTitle
+//         }
+//         slug
+//         title
+//         originalId
+//       }
+//     }
+//   `)
+//    console.log(result) 
+//   // result.data.allDatoCmsHomepage = async () =>{
+//   //   createPage({
+//   //     path: `/${page.slug === "index" ? "" : page.slug}`,
+//   //     component: require.resolve("./src/templates/generic-page.js"),
+//   //     context: {
+//   //       pageId: page.originalId,
+//   //     },
+//   //   })
+//   // }
+// }
