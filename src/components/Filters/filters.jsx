f1d140238f37822c5538a4ad46cf98586ebe6554
PaulZuev <pavel.z@ramotion.agency>
2022-03-25 18:24:14 +0300

    mino fixed

--- src/components/Filters/filters.jsx
+++ src/components/Filters/filters.jsx
@@ -4,8 +4,8 @@ import { useState } from 'react';
 import Icon from '../../images/icon.svg'
 
 const style = {
-  button : "inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500",
-  box : "z-10 whitespace-nowrap origin-top-right absolute mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+  button : "inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 whitespace-nowrap",
+  box : "z-10 origin-top-right absolute mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
 }
 
 const Filters = (props) =>{
